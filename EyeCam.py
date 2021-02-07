f71febfba129111f4cd7cb9ae11aee1b16ed736f
rytnux <68074768+fuckwbored@users.noreply.github.com>
2021-02-07 19:18:03 +0200

    Add files via upload

---
+++ EyeCam.py
@@ -0,0 +1,107 @@
+import os
+os.system('clear')
+
+print(f"""
+
+
+   ('-.                 ('-.               ('-.     _   .-')    
+ _(  OO)              _(  OO)             ( OO ).-.( '.( OO )_  
+(,------. ,--.   ,--.(,------.   .-----.  / . --. / ,--.   ,--.)
+ |  .---'  \  `.'  /  |  .---'  '  .--./  | \-.  \  |   `.'   | 
+ |  |    .-')     /   |  |      |  |('-..-'-'  |  | |         | 
+(|  '--.(OO  \   /   (|  '--.  /_) |OO  )\| |_.'  | |  |'.'|  | 
+ |  .--' |   /  /\_   |  .--'  ||  |`-'|  |  .-.  | |  |   |  | 
+ |  `---.`-./  /.__)  |  `---.(_'  '--'\  |  | |  | |  |   |  | 
+ `------'  `--'       `------'   `-----'  `--' `--' `--'   `--' 
+
+
+""")                               
+print("Please select cameras :")
+print("""
+1. AXIS                        
+2. WebcamXP (Webcam and Network Camera Security Software)
+3. Sony 
+4. Panasonic
+
+5. ABOUT AUTHOR
+
+https://www.donationalerts.com/r/rytnux - support me
+by Opostal :)
+
+Version 1.0
+""")
+num = int(input("select : "))
+if num == 1:                                                                                                                                                                                                                      
+    print ("AXIS NETWORK CAMERAS")
+
+    print("91.133.85.98")
+    print("108.161.54.81")
+    print("46.26.42.148:8090")
+    print("194.14.32.91")
+    print("46.14.58.189")
+    print("69.193.53.42:9000")
+    print("81.62.149.154:25500")
+    print("80.112.227.178:7777")
+    print("79.141.89.38")
+    print("198.82.159.134")
+    print("2.229.43.7:9005")
+    print("80.254.191.189:8008")
+    print("46.252.49.2:8082")
+    print("62.238.142.241:8081")
+    print("210.172.201.69/Top")
+    print("https://webcam.nmc.edu/#view")
+    print("http://grovecam.iup.edu")
+    print("http://webcam.fitjar.kommune.no")
+    print("http://teamkleijn.nl:83")
+    print("81.82.201.132")
+    print("bit.ly/3oXOBjM")
+    print("http://avptcam.uconn.edu")
+    print("195.196.35.91")
+elif num ==2:
+    print ("WebcamXP Network Cameras")
+
+    print ("86.61.65.77:8081")
+    print ("99.132.182.35:8080")
+    print ("201.236.147.2:8080")
+    print("173.18.90.193:8080")
+    print("109.233.191.130:8080")	
+    print("80.98.84.83:8080")
+    print("73.49.126.240:8084")
+    print("65.102.199.230:8080")
+    print("http://f5kob.no-ip.info:5100")
+    print("178.124.145.121:8080")
+    print("85.159.104.210:8080")
+    print("77.94.120.22:8080")
+    print("82.77.16.188:8080")
+    print("12.87.20.234:9090")
+    print("193.138.213.203:8081")
+    print("85.93.53.175:8080")
+    print("82.207.41.104:8080")
+    print("91.233.46.24:8011")
+elif num ==3:
+    print("SONY NETWORK CAMERAS")
+    print("70.91.205.29/home/homeS.html")
+    print("http://ailes-de-persan.dyndns.org:1080/home/homeS.html")
+    print("87.202.33.22:8008/home/homeJ.html")
+    print("http://ailes-de-persan.dyndns.org:1080/home/homeS.html")
+    print("203.95.10.99:1025/home/homeJ.html")
+elif num ==4:
+    print("PANASONIC NETWORK CAMERAS")
+    print("69.71.168.195")
+    print("24.155.92.234:50090")
+    print("68.177.73.106:81")
+    print("66.91.133.142:88")
+    print("173.255.109.205")
+    print("61.211.241.239")
+    print("100.37.240.26:91")
+    print("75.145.165.113:86")
+    print("125.52.82.245:82")
+    print("61.211.241.239")
+elif num==5:
+    print("My Instagram: @rytnux")
+    print("Telegram channel: t.me/termuxqew")
+    print("Contact Me: t.me/opostal1")
+
+   
+
+  
