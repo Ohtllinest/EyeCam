87c72b53908c4b1ec8e2011f25737c841692892f
rytnux <68074768+fuckwbored@users.noreply.github.com>
2021-02-08 20:27:01 +0200

    Add files via upload

---
+++ EyeCam.py
@@ -0,0 +1,232 @@
+import os
+import time
+os.system('clear')
+
+from termcolor import colored
+print(f"""
+
+
+   ('-.                 ('-.               ('-.     _   .-')    
+ _(  OO)              _(  OO)             ( OO ).-.( '.( OO )_  
+(,------. ,--.   ,--.(,------.   .-----.  / . --. / ,--.   ,--.)
+ |  .---'  \  `.'  /  |  .---'  '  .--./  | \-.  \  |   `.'   |   | Version 1.1                     |
+ |  |    .-')     /   |  |      |  |('-..-'-'  |  | |         |   |--By Opostal--                   |
+(|  '--.(OO  \   /   (|  '--.  /_) |OO  )\| |_.'  | |  |'.'|  |   |Telegram channel: t.me/termuxqew | 
+ |  .--' |   /  /\_   |  .--'  ||  |`-'|  |  .-.  | |  |   |  |  
+ |  `---.`-./  /.__)  |  `---.(_'  '--'\  |  | |  | |  |   |  | 
+ `------'  `--'       `------'   `-----'  `--' `--' `--'   `--' 
+
+
+""")
+from termcolor import colored                               
+print(colored("Network Cameras IPgiver", 'red'))
+print("""
+1. AXIS                        
+2. WebcamXP (Webcam and Network Camera Security Software)
+3. Sony 
+4. Panasonic
+5. Bosch
+6. Mobotix
+7. D-Link
+8. Canon
+9. MJPG-streamer team
+10. YawCam (Wbcam software)
+11. (No Name)
+0 EXIT
+
+
+https://www.donationalerts.com/r/rytnux - support me
+
+""")
+num = int(input("select : "))
+if num == 1: 
+    os.system('clear')
+                               
+    print("AXIS NETWORK CAMERAS")
+    print("91.133.85.98")
+    print("108.161.54.81")
+    print("46.26.42.148:8090")
+    print("194.14.32.91")
+    print("46.14.58.189")
+    time.sleep(0.4)
+    print("69.193.53.42:9000")
+    print("81.62.149.154:25500")
+    print("80.112.227.178:7777")
+    print("79.141.89.38")
+    print("198.82.159.134")
+    time.sleep(0.4)
+    print("2.229.43.7:9005")
+    print("80.254.191.189:8008")
+    print("46.252.49.2:8082")
+    print("62.238.142.241:8081")
+    print("210.172.201.69/Top")
+    time.sleep(0.4)
+    print("https://webcam.nmc.edu/#view")
+    print("http://grovecam.iup.edu")
+    print("http://webcam.fitjar.kommune.no")
+    print("http://teamkleijn.nl:83")
+    print("81.82.201.132")
+    print("bit.ly/3oXOBjM")
+    print("http://avptcam.uconn.edu")
+    print("195.196.35.91")
+    print("79.152.109.55:80")
+    print("2.136.229.22:8084")
+    time.sleep(0.4)
+    print("195.235.161.212:8090")
+    print("83.247.140.189:8001")
+    print("2.138.120.138:90")
+    print("2.139.223.112:8090")
+    print("188.86.157.85:80")
+    time.sleep(0.4)
+    print("188.86.157.89:80")
+    print("188.86.157.90:80")
+    print("188.86.157.86:80")
+    print("213.96.204.122:8080") 
+    print("213.96.204.122:808")
+    time.sleep(0.4)
+    print("46.253.34.116:85") 
+    print("195.53.238.58:80") 
+    print("185.32.19.218:85") 
+    print("83.48.52.76:8083")
+    print("83.36.56.35:82")
+    print("80.36.61.219:80")
+    print("80.28.111.68:81")
+    print("83.36.56.35:81")
+    print("158.42.148.154")
+    time.sleep(0.3)
+    print("Exiting...")
+    time.sleep(0.8)
+
+if num ==2:
+    print("WEBCAMXP NETWORK CAMERAS")
+
+    print("86.61.65.77:8081")
+    print("99.132.182.35:8080")
+    print("201.236.147.2:8080")
+    print("173.18.90.193:8080")
+    print("109.233.191.130:8080")
+    time.sleep(0.4)	
+    print("80.98.84.83:8080")
+    print("73.49.126.240:8084")
+    print("65.102.199.230:8080")
+    print("http://f5kob.no-ip.info:5100")
+    print("178.124.145.121:8080")
+    time.sleep(0.4)
+    print("85.159.104.210:8080")
+    print("77.94.120.22:8080")
+    print("82.77.16.188:8080")
+    print("12.87.20.234:9090")
+    print("193.138.213.203:8081")
+    time.sleep(0.4)
+    print("85.93.53.175:8080")
+    print("82.207.41.104:8080")
+    print("91.233.46.24:8011")
+    print("83.45.84.53:80")
+    time.sleep(0.3)
+    print("Exiting...")
+    time.sleep(0.8)
+elif num ==3:
+    os.system('clear')
+    print("SONY NETWORK CAMERAS")
+    print("70.91.205.29/home/homeS.html")
+    print("http://ailes-de-persan.dyndns.org:1080/home/homeS.html")
+    print("87.202.33.22:8008/home/homeJ.html")
+    print("http://ailes-de-persan.dyndns.org:1080/home/homeS.html")
+    print("203.95.10.99:1025/home/homeJ.html")
+elif num ==4:
+    print("PANASONIC NETWORK CAMERAS")
+    print("69.71.168.195")
+    print("24.155.92.234:50090")
+    print("68.177.73.106:81")
+    print("66.91.133.142:88")
+    print("173.255.109.205")
+    time.sleep(0.4)
+    print("61.211.241.239")
+    print("100.37.240.26:91")
+    print("75.145.165.113:86")
+    print("125.52.82.245:82")
+    print("61.211.241.239")
+    time.sleep(0.4)
+    print("80.29.127.26:81")
+    print("80.28.137.190:83")
+    print("80.29.127.26:83")
+    time.sleep(0.3)
+    print("Exiting...")
+    time.sleep(0.8)
+elif num==5:
+    os.system('clear')
+
+    print("BOSCH NETWORK CAMERAS")
+    print("88.12.9.90:10000")
+    print("85.62.59.215:10000")
+    print("80.24.96.159:84")
+    print("80.24.96.159:82")
+    print("188.127.182.43:80")
+    time.sleep(0.4)
+    print("95.210.44.56:8084")
+    time.sleep(0.3)
+    print("Exiting...")
+    time.sleep(0.8)
+elif num==6:
+    os.system('clear')
+
+    print("MOBOTIX NETWORK CAMERAS")
+    print("95.63.46.33:81")
+    print("88.5.231.28:8001")
+    print("83.61.182.89:85")
+    print("80.32.117.104:2000")
+    print("185.49.169.66:1024")
+    time.sleep(0.4)
+    print("195.55.147.66:86")
+    print("150.214.217.118:8080")
+    print("80.24.34.206:80")
+    print("37.130.145.254:8080")
+    print("185.49.168.63:8090")
+    time.sleep(0.4)
+    print("212.145.143.135")
+    time.sleep(0.3)
+    print("Exiting...")
+    time.sleep(0.8)
+elif num==7:
+    os.system('clear')
+
+    print("D-LINK NETWORK CAMERAS")
+    print("2.153.108.213:80")
+    time.sleep(0.3)
+    print("Exiting...")
+    time.sleep(0.8)
+elif num==8:
+    os.system('clear')
+
+    print("CANON NETWORK CAMERAS")
+    print("212.8.110.23:80")
+    print("83.235.181.98:81")
+    time.sleep(0.3)
+    print("Exiting...")
+    time.sleep(0.8)
+elif num==9:
+    os.system('clear')
+
+    print("MJPG-streamer team NETWORK CAMERAS")
+    print("83.46.20.45:8080")
+    print("81.32.144.59:8081")
+    print("83.36.99.76:8080")
+    time.sleep(0.3)
+    print("Exiting...")
+    time.sleep(0.8)
+elif num==10:
+    os.system('clear')
+    print("YAWCAM")
+    print("185.147.16.162:8888")
+    time.sleep(0.3)
+    print("Exiting...")
+    time.sleep(0.8)
+elif num==11:
+    os.system('clear')
+    print("IDK NAME OF THIS CAMERAS XD")
+    print("46.253.34.116:80")
+    print("213.99.88.41:8080 (cameras of ship  FEDELLO II)")
+    print("155.207.169.86:8080")
+    time.sleep(0.3)
+    print("Exiting...")
+    time.sleep(0.8)
